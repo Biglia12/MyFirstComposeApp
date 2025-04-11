edcdbeb459450c20eb041c5f9a29df7ff2979466
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2025-04-11 13:12:20 -0300

    row

--- app/src/main/java/com/allapps/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/allapps/myfirstcomposeapp/MainActivity.kt
@@ -7,12 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.allapps.myfirstcomposeapp.components.layout.MyBox
-import com.allapps.myfirstcomposeapp.components.layout.MyColumn
+import com.allapps.myfirstcomposeapp.components.layout.MyRow
 import com.allapps.myfirstcomposeapp.login.Greeting
 import com.allapps.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
@@ -23,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstComposeAppTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                   MyColumn(Modifier.padding(innerPadding))
+                   MyRow(Modifier.padding(innerPadding))
                 }
             }
         }
---
+++ app/src/main/java/com/allapps/myfirstcomposeapp/components/layout/MyRow.kt
@@ -0,0 +1,102 @@
+package com.allapps.myfirstcomposeapp.components.layout
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun MyRow(modifier: Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
+    ) {
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Red)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Gray)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Green)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Blue)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Yellow)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Red)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Gray)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Green)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Blue)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Yellow)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Red)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Gray)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Green)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Blue)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Yellow)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Red)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Gray)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Green)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Blue)
+        )
+        Text(
+            "Hola 1", modifier = Modifier
+                .background(Color.Yellow)
+        )
+    }
+}
