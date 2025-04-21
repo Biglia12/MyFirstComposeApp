acd9ba44faef90899b189b422b02d0196a374e49
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2025-04-21 16:29:03 -0300

    spacer

--- app/src/main/java/com/allapps/myfirstcomposeapp/components/layout/exercise1/ExerciseOne.kt
+++ app/src/main/java/com/allapps/myfirstcomposeapp/components/layout/exercise1/ExerciseOne.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,7 +30,10 @@ fun ExerciseOne(modifier: Modifier = Modifier) {
                 .fillMaxWidth()
                 .background(Color.Cyan),
             contentAlignment = Alignment.Center
-        ) { Text("Ejemplo1", modifier = Modifier.background(Color.Gray)) }
+        ) {
+            Text("Ejemplo1", modifier = Modifier.background(Color.Gray))
+        }
+        Spacer(modifier = Modifier.height(20.dp))
 
         Row(modifier = Modifier
             .weight(1f)
@@ -39,6 +45,7 @@ fun ExerciseOne(modifier: Modifier = Modifier) {
                     .background(Color.Red),
                 contentAlignment = Alignment.Center
             ) { Text("Ejemplo2", modifier = Modifier.background(Color.Gray)) }
+            Spacer(modifier = Modifier.width(30.dp))
 
             Box(
                 modifier = Modifier
@@ -48,6 +55,7 @@ fun ExerciseOne(modifier: Modifier = Modifier) {
                 contentAlignment = Alignment.Center
             ) { Text("Ejemplo3", modifier = Modifier.background(Color.Gray)) }
         }
+
         Box(
             modifier = Modifier
                 .weight(1f)
